d98826a1dcb6b8766e553ecffd830cf54f2dba82
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2025-03-15 23:24:08 +0600

    fixed reviews

--- constant/reviews.js
+++ constant/reviews.js
@@ -1,21 +1,41 @@
+export const reviews_ = [
+  {
+    src: "http://localhost:3000/images/review_1.jpg",
+  },
+  {
+    src: "http://localhost:3000/images/review_1.jpg",
+  },
+  {
+    src: "http://localhost:3000/images/review_1.jpg",
+  },
+  {
+    src: "http://localhost:3000/images/review_1.jpg",
+  },
+  {
+    src: "http://localhost:3000/images/review_1.jpg",
+  },
+  {
+    src: "http://localhost:3000/images/review_1.jpg",
+  },
+];
 export const reviews = [
   {
-    src: "http://localhost:3000/images/review_1.jpg",
+    src: "https://fiora-one.vercel.app/images/review_1.jpg",
   },
   {
-    src: "http://localhost:3000/images/review_1.jpg",
+    src: "https://fiora-one.vercel.app/images/review_1.jpg",
   },
   {
-    src: "http://localhost:3000/images/review_1.jpg",
+    src: "https://fiora-one.vercel.app/images/review_1.jpg",
   },
   {
-    src: "http://localhost:3000/images/review_1.jpg",
+    src: "https://fiora-one.vercel.app/images/review_1.jpg",
   },
   {
-    src: "http://localhost:3000/images/review_1.jpg",
+    src: "https://fiora-one.vercel.app/images/review_1.jpg",
   },
   {
-    src: "http://localhost:3000/images/review_1.jpg",
+    src: "https://fiora-one.vercel.app/images/review_1.jpg",
   },
 ];
 
